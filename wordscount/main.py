0a44fc1204d5a2b5f238c026c3c6f15ae13ee815
Solhee Tucker <solheepark29@gmail.comsolheepark29@gmail.comsolheepark29@gmail.com>
2024-05-30 17:15:09 -0700

    patterns to remove

--- wordscount/main.py
+++ wordscount/main.py
@@ -1,11 +1,12 @@
 # Imports
 import heapq
 import os
+import re
 from collections import Counter
 
 
 # Functions
-def read_txt_files_from_dir():
+def get_txt_list_from_dir():
     text_sources_dir = os.path.dirname(os.path.dirname(__file__))
     for _ in range(2):
         text_sources_dir = os.path.dirname(text_sources_dir)
@@ -16,26 +17,34 @@ def read_txt_files_from_dir():
     text_files = [file for file in file_list if file.endswith('.txt')]
     text_list = []
     for filename in text_files:
-        text_list.append(read_txt(text_sources_dir, filename))
+        text_list.extend(read_txt(text_sources_dir, filename))
     return text_list
 
 
-def read_txt(file_path, text_file):
-    with open(os.path.join(file_path, text_file), 'r', encoding='utf-8') as file:
-        text_list = file.readlines()
-    return text_list
+def read_txt(file_path, file_name):
+    """
+    input : file path, file_name
+    output : list of words in file
+    """
+    word_list = []
+    pattern = r'[0-9\n\t.,?!;:(){}\[\]\'\"\\-]|\\n|\\t'
+    with open(os.path.join(file_path, file_name), 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+    for line in lines:
+        cleaned_line = re.sub(pattern, ' ', line)
+        words = cleaned_line.split()
+        filtered_words = [word.lower() for word in words if len(word) > 1]
+        word_list.extend(filtered_words)
+    return word_list
+
 
 def pdf_to_txt(file_path):
     return
 
 
-def count_word_frequency(text_list, top_n=None):
+def count_word_frequency(words_list, top_n=None):
     word_counts = Counter()
-    for text in text_list:
-        for line in text:
-            words = line.split()
-            words = [word.lower() for word in words]  # Convert words to lower case
-            word_counts.update(words)
+    word_counts.update(words_list)
 
     # Convert the Counter to a list of tuples and use a heap to keep top N frequent words
     heap = []
@@ -54,6 +63,7 @@ def count_word_frequency(text_list, top_n=None):
     return sorted_word_counts
 
 
-txt_list = read_txt_files_from_dir()
-wc = count_word_frequency(txt_list)
+# Starts here
+words_list = get_txt_list_from_dir()
+wc = count_word_frequency(words_list)
 print(wc)
